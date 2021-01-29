1973a36c3f324c570e7d7f2d32b18ebb1fec5f60
Juanma Tola <tolajuanmanuel@yahoo.com.ar>
2021-01-29 18:30:18 -0300

    Refactorizo main (A exportar en modulo externo)

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
-import * as paypal from 'https://www.paypal.com/sdk/js?client-id=sb&currency=EUR';
 import Arcticle from './modules/arcticle_module.js';
 import initPayPalButtons from './modules/paypal_buttons/paypal_buttons.js';
+function sale(plan, buyer) {
+    console.log(`Payment received correctly.`);
+    
+    let arcticle = new Arcticle(plan);
+    let res = arcticle.sendTo(buyer);
+    console.log(`Sending request.`);
+
+    console.log(res);
+}
 
 let buttonsIds = ['paypal-button-1', 'paypal-button-2', 'paypal-button-3']; 
 
-let prueba = initPayPalButtons(buttonsIds);
+initPayPalButtons(buttonsIds);
 
 let paypalButton1 = document.getElementById('paypal-button-1');
 let paypalButton2 = document.getElementById('paypal-button-2');
 let paypalButton3 = document.getElementById('paypal-button-3');
 
-paypalButton1.addEventListener('onapprove', (e) =>{
-    let arcticle = new Arcticle('Plan 1', e.detail.name, e.detail.email);
-    arcticle.send();
+paypalButton1.addEventListener('onapprove', (pay) =>{
+    sale('plan1', pay.detail);
 })
 
-paypalButton2.addEventListener('onapprove', (e) =>{
-    let arcticle = new Arcticle('Plan 2', e.detail.name, e.detail.email);
-    arcticle.send();
+paypalButton2.addEventListener('onapprove', (pay) =>{
+    sale('plan2', pay.detail);
 })
 
-paypalButton3.addEventListener('onapprove', (e) =>{
-    let arcticle = new Arcticle('Plan 3', e.detail.name, e.detail.email);
-    arcticle.send();
+paypalButton3.addEventListener('onapprove', (pay) =>{
+    sale('plan3', pay.detail);
 })
